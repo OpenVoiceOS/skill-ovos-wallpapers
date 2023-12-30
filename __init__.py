5b43bbd0c6e1c5d24ab85743a82438cd2625619c
JarbasAi <jarbasai@mailfence.com>
2023-12-30 02:47:34 +0000

    update imports

--- __init__.py
+++ __init__.py
@@ -1,23 +1,18 @@
-from itertools import chain
-from os.path import join, exists
-from mycroft.skills.skill_data import read_vocab_file
-from mycroft.util import resolve_resource_file
 import random
-from mycroft.util import camel_case_split, create_daemon
-from mycroft import MycroftSkill, intent_file_handler, intent_handler
-from mycroft.skills.core import resting_screen_handler
-from wallpaper_changer.search import latest_reddit, latest_wpcraft, \
-    random_unsplash, latest_unsplash
+
+from ovos_utils import camel_case_split
+from ovos_workshop.intents import IntentBuilder
+from ovos_utils.parse import match_one
+from ovos_workshop.decorators import intent_handler, resting_screen_handler
+from ovos_workshop.skills import OVOSSkill
 from wallpaper_changer import set_wallpaper, get_desktop_environment
-from mtranslate import translate
-import re
-from mycroft.util.parse import match_one
-from adapt.intent import IntentBuilder
+from wallpaper_changer.search import latest_reddit, latest_wpcraft, \
+    latest_unsplash
 
 
-class WallpapersSkill(MycroftSkill):
-    def __init__(self):
-        super(WallpapersSkill, self).__init__(name='Wallpapers')
+class WallpapersSkill(OVOSSkill):
+
+    def initialize(self):
         # skill settings defaults
         if "auto_detect" not in self.settings:
             self.settings["auto_detect"] = True
@@ -35,63 +30,63 @@ class WallpapersSkill(MycroftSkill):
             self.settings["wpcraft"] = True
 
         subs = ['/r/EarthPorn', '/r/BotanicalPorn', '/r/WaterPorn',
-                    '/r/SeaPorn',
-                    '/r/SkyPorn', '/r/FirePorn', '/r/DesertPorn',
-                    '/r/WinterPorn',
-                    '/r/AutumnPorn', '/r/WeatherPorn', '/r/GeologyPorn',
-                    '/r/SpacePorn',
-                    '/r/BeachPorn', '/r/MushroomPorn', '/r/SpringPorn',
-                    '/r/SummerPorn',
-                    '/r/LavaPorn', '/r/LakePorn', '/r/CityPorn',
-                    '/r/VillagePorn',
-                    '/r/RuralPorn', '/r/ArchitecturePorn', '/r/HousePorn',
-                    '/r/CabinPorn',
-                    '/r/ChurchPorn', '/r/AbandonedPorn', '/r/CemeteryPorn',
-                    '/r/InfrastructurePorn', '/r/MachinePorn', '/r/CarPorn',
-                    '/r/F1Porn',
-                    '/r/MotorcyclePorn', '/r/MilitaryPorn', '/r/GunPorn',
-                    '/r/KnifePorn',
-                    '/r/BoatPorn', '/r/RidesPorn', '/r/DestructionPorn',
-                    '/r/ThingsCutInHalfPorn', '/r/StarshipPorn',
-                    '/r/ToolPorn',
-                    '/r/TechnologyPorn', '/r/BridgePorn', '/r/PolicePorn',
-                    '/r/SteamPorn',
-                    '/r/RetailPorn', '/r/SpaceFlightPorn', '/r/roadporn',
-                    '/r/drydockporn',
-                    '/r/AnimalPorn', '/r/HumanPorn', '/r/EarthlingPorn',
-                    '/r/AdrenalinePorn', '/r/ClimbingPorn', '/r/SportsPorn',
-                    '/r/AgriculturePorn', '/r/TeaPorn', '/r/BonsaiPorn',
-                    '/r/FoodPorn',
-                    '/r/CulinaryPorn', '/r/DessertPorn', '/r/DesignPorn',
-                    '/r/RoomPorn',
-                    '/r/AlbumArtPorn', '/r/MetalPorn', '/r/MoviePosterPorn',
-                    '/r/TelevisionPosterPorn', '/r/ComicBookPorn',
-                    '/r/StreetArtPorn',
-                    '/r/AdPorn', '/r/ArtPorn', '/r/FractalPorn',
-                    '/r/InstrumentPorn',
-                    '/r/ExposurePorn', '/r/MacroPorn', '/r/MicroPorn',
-                    '/r/GeekPorn',
-                    '/r/MTGPorn', '/r/GamerPorn', '/r/PowerWashingPorn',
-                    '/r/AerialPorn',
-                    '/r/OrganizationPorn', '/r/FashionPorn', '/r/AVPorn',
-                    '/r/ApocalypsePorn', '/r/InfraredPorn', '/r/ViewPorn',
-                    '/r/HellscapePorn', '/r/sculptureporn', '/r/HistoryPorn',
-                    '/r/UniformPorn', '/r/BookPorn', '/r/NewsPorn',
-                    '/r/QuotesPorn',
-                    '/r/FuturePorn', '/r/FossilPorn', '/r/MegalithPorn',
-                    '/r/ArtefactPorn',
-                    '/r/AmateurEarthPorn', '/r/AmateurPhotography',
-                    '/r/ArtistOfTheDay',
-                    '/r/BackgroundArt', '/r/Conservation', '/r/EarthPornVids',
-                    '/r/EyeCandy', '/r/FWEPP', '/r/ImaginaryLandscapes',
-                    '/r/ImaginaryWildlands', '/r/IncredibleIndia',
-                    '/r/ITookAPicture',
-                    '/r/JoshuaTree', '/r/NationalGeographic', '/r/Nature',
-                    '/r/NatureGifs',
-                    '/r/NaturePics', '/r/NotSafeForNature', '/r/NZPhotos',
-                    '/r/remoteplaces', '/r/Schweiz', '/r/SpecArt',
-                    '/r/wallpapers',
-                    "/r/InterstellarArt"]
+                '/r/SeaPorn',
+                '/r/SkyPorn', '/r/FirePorn', '/r/DesertPorn',
+                '/r/WinterPorn',
+                '/r/AutumnPorn', '/r/WeatherPorn', '/r/GeologyPorn',
+                '/r/SpacePorn',
+                '/r/BeachPorn', '/r/MushroomPorn', '/r/SpringPorn',
+                '/r/SummerPorn',
+                '/r/LavaPorn', '/r/LakePorn', '/r/CityPorn',
+                '/r/VillagePorn',
+                '/r/RuralPorn', '/r/ArchitecturePorn', '/r/HousePorn',
+                '/r/CabinPorn',
+                '/r/ChurchPorn', '/r/AbandonedPorn', '/r/CemeteryPorn',
+                '/r/InfrastructurePorn', '/r/MachinePorn', '/r/CarPorn',
+                '/r/F1Porn',
+                '/r/MotorcyclePorn', '/r/MilitaryPorn', '/r/GunPorn',
+                '/r/KnifePorn',
+                '/r/BoatPorn', '/r/RidesPorn', '/r/DestructionPorn',
+                '/r/ThingsCutInHalfPorn', '/r/StarshipPorn',
+                '/r/ToolPorn',
+                '/r/TechnologyPorn', '/r/BridgePorn', '/r/PolicePorn',
+                '/r/SteamPorn',
+                '/r/RetailPorn', '/r/SpaceFlightPorn', '/r/roadporn',
+                '/r/drydockporn',
+                '/r/AnimalPorn', '/r/HumanPorn', '/r/EarthlingPorn',
+                '/r/AdrenalinePorn', '/r/ClimbingPorn', '/r/SportsPorn',
+                '/r/AgriculturePorn', '/r/TeaPorn', '/r/BonsaiPorn',
+                '/r/FoodPorn',
+                '/r/CulinaryPorn', '/r/DessertPorn', '/r/DesignPorn',
+                '/r/RoomPorn',
+                '/r/AlbumArtPorn', '/r/MetalPorn', '/r/MoviePosterPorn',
+                '/r/TelevisionPosterPorn', '/r/ComicBookPorn',
+                '/r/StreetArtPorn',
+                '/r/AdPorn', '/r/ArtPorn', '/r/FractalPorn',
+                '/r/InstrumentPorn',
+                '/r/ExposurePorn', '/r/MacroPorn', '/r/MicroPorn',
+                '/r/GeekPorn',
+                '/r/MTGPorn', '/r/GamerPorn', '/r/PowerWashingPorn',
+                '/r/AerialPorn',
+                '/r/OrganizationPorn', '/r/FashionPorn', '/r/AVPorn',
+                '/r/ApocalypsePorn', '/r/InfraredPorn', '/r/ViewPorn',
+                '/r/HellscapePorn', '/r/sculptureporn', '/r/HistoryPorn',
+                '/r/UniformPorn', '/r/BookPorn', '/r/NewsPorn',
+                '/r/QuotesPorn',
+                '/r/FuturePorn', '/r/FossilPorn', '/r/MegalithPorn',
+                '/r/ArtefactPorn',
+                '/r/AmateurEarthPorn', '/r/AmateurPhotography',
+                '/r/ArtistOfTheDay',
+                '/r/BackgroundArt', '/r/Conservation', '/r/EarthPornVids',
+                '/r/EyeCandy', '/r/FWEPP', '/r/ImaginaryLandscapes',
+                '/r/ImaginaryWildlands', '/r/IncredibleIndia',
+                '/r/ITookAPicture',
+                '/r/JoshuaTree', '/r/NationalGeographic', '/r/Nature',
+                '/r/NatureGifs',
+                '/r/NaturePics', '/r/NotSafeForNature', '/r/NZPhotos',
+                '/r/remoteplaces', '/r/Schweiz', '/r/SpecArt',
+                '/r/wallpapers',
+                "/r/InterstellarArt"]
         self.subs = [s.split("/")[-1].strip() for s in subs]
         self.wpcats = [
             '3d', 'abstract', 'animals', 'anime', "art", "black", "cars",
@@ -113,15 +108,8 @@ class WallpapersSkill(MycroftSkill):
         # state trackers
         self.pic_idx = 0
         self.picture_list = []
-        self.translate_cache = {}
-        self._old_settings = dict(self.settings)
 
         # events
-        self.settings_change_callback = self._on_web_settings_change
-        self.namespace = self.__class__.__name__.lower()
-        self.skill_name = camel_case_split(self.__class__.__name__)
-
-    def initialize(self):
         self.log.info("Detected desktop env " + self.settings["desktop_env"])
 
         # gui slideshow buttons
@@ -130,18 +118,11 @@ class WallpapersSkill(MycroftSkill):
         self.gui.register_handler('skill-wallpapers.jarbasskills.prev',
                                   self.handle_prev)
 
-        # bigscreen homepage
-        self.add_event('skill-wallpapers.jarbasskills.home',
-                       self.homepage)
-
     # on install
     def get_intro_message(self):
         # welcome dialog on skill install
-        self.speak_dialog("intro", {"skill_name": self.skill_name})
-
-    # bigscreen
-    def homepage(self):
-        self.handle_random_picture()
+        self.speak_dialog("intro",
+                          {"skill_name": camel_case_split(self.__class__.__name__)})
 
     # idle screen
     def update_picture(self, query=None):
@@ -180,7 +161,7 @@ class WallpapersSkill(MycroftSkill):
             best_sub = query
             best_score = 0
             for s in self.subs:
-                words = self.get_voc(s)
+                words = self.voc_list(s, self.lang)
                 sub, score = match_one(query, words)
                 if score > best_score:
                     best_sub = sub
@@ -219,7 +200,6 @@ class WallpapersSkill(MycroftSkill):
             self.picture_list = wps
             self.pic_idx = 0
             data["url"] = "https://www.reddit.com/r/wallpapers/"
-        data = self._tx(data)
 
         for k in data:
             self.gui[k] = data[k]
@@ -232,49 +212,6 @@ class WallpapersSkill(MycroftSkill):
         self.gui.show_image(image, fill='PreserveAspectFit')
 
     # skill internals
-    def get_voc(self, voc_filename):
-        # Check for both skill resources and mycroft-core resources
-        voc = self.find_resource(voc_filename + '.voc', 'vocab')
-        if not voc:  # Check for vocab in mycroft core resources
-            voc = resolve_resource_file(join('text', self.lang,
-                                             voc_filename + '.voc'))
-
-        if not voc or not exists(voc):
-            raise FileNotFoundError(
-                'Could not find {}.voc file'.format(voc_filename))
-        # load vocab and flatten into a simple list
-        vocab = read_vocab_file(voc)
-        return list(chain(*vocab))
-
-    def _tx(self, data, base_lang="en"):
-        keys = ["title"]
-        for k in keys:
-            if k not in data:
-                continue
-            data[k] = re.sub(r'\[.*?\]', '', data[k])
-            data[k] = re.sub(r'\{.*?\}', '', data[k])
-            data[k] = re.sub(r'\(.*?\)', '', data[k])
-            data[k] = data[k].strip()
-            if self.lang.split("-")[0] != base_lang:
-                if data[k] not in self.translate_cache:
-                    translated = translate(data[k], self.lang)
-                    self.translate_cache[data[k]] = translated
-                    data[k] = translated
-                else:
-                    data[k] = self.translate_cache[data[k]]
-        return data
-
-    def _on_web_settings_change(self):
-        for k in self.settings:
-            if self.settings[k] != self._old_settings[k]:
-                self.handle_new_setting(k, self.settings[k],
-                                        self._old_settings[k])
-        self._old_settings = dict(self.settings)
-
-    def handle_new_setting(self, key, value, old_value):
-        self.log.debug("{name}: {key} changed from {value} to {old}".format(
-            key=key, value=value, old=old_value, name=self.skill_name))
-
     # skill functionality
     def change_wallpaper(self, image):
         if self.settings["auto_detect"]:
@@ -298,7 +235,7 @@ class WallpapersSkill(MycroftSkill):
         self.set_context("SlideShow")
 
     # intents
-    @intent_file_handler("wallpaper.random.intent")
+    @intent_handler("wallpaper.random.intent")
     def handle_random_wallpaper(self, message):
         image, title = self.update_picture()
         success = self.change_wallpaper(image)
@@ -308,7 +245,7 @@ class WallpapersSkill(MycroftSkill):
             self.speak_dialog("wallpaper fail")
         self.gui.show_image(image, fill='PreserveAspectFit')
 
-    @intent_file_handler("picture.random.intent")
+    @intent_handler("picture.random.intent")
     def handle_random_picture(self, message=None):
         self.update_picture()
         title = self.picture_list[self.pic_idx].get("title")
@@ -316,7 +253,7 @@ class WallpapersSkill(MycroftSkill):
             self.speak(title)
         self.display()
 
-    @intent_file_handler("wallpaper.about.intent")
+    @intent_handler("wallpaper.about.intent")
     def handle_wallpaper_about(self, message):
         query = message.data["query"]
         self.speak_dialog("searching", {"query": query})
@@ -328,7 +265,7 @@ class WallpapersSkill(MycroftSkill):
             self.speak_dialog("wallpaper fail")
         self.gui.show_image(image, fill='PreserveAspectFit')
 
-    @intent_file_handler("picture.about.intent")
+    @intent_handler("picture.about.intent")
     def handle_picture_about(self, message=None):
         query = message.data["query"]
         self.speak_dialog("searching", {"query": query})
@@ -376,5 +313,3 @@ class WallpapersSkill(MycroftSkill):
             self.speak_dialog("wallpaper fail")
 
 
-def create_skill():
-    return WallpapersSkill()
